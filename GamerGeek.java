cdaa22ec6f6e3aa48dfe13b7bf22f1d3f145e764
RiseofDoom <42861071+RiseofDoom@users.noreply.github.com>
2022-08-29 18:49:20 -1000

    Add files via upload

---
+++ GamerGeek.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uhh.lab1;
+
+public class GamerGeek extends GenericNerd {
+    
+    public int console;
+    public String consoleName;
+    
+    public GamerGeek(String name, int console, int nerdFactor)
+    {
+        super(name,nerdFactor);
+        if(name == null)
+        {
+            System.out.println("Error: GamerGeek constructor - name is null!");
+            System.exit(1);
+        }
+        
+        if(console == 1 && nerdFactor < 5 || nerdFactor < 1 || nerdFactor > 10)
+        {
+            System.out.println("Warning: Nerd factor for PC gamer must be above 5 (using default 5");
+            this.nerdFactor = 5;
+        }
+        else if(console == 3  && nerdFactor > 6 || nerdFactor < 1 || nerdFactor > 10)
+                {
+                    System.out.println("Warning: Nerd factor for Xbox gamer must be between 1 and 6 (using default 6");
+                    this.nerdFactor = 6;
+                }
+        
+       switch(console)
+       {
+           case 1:
+               this.consoleName = "PC";
+               break;
+           case 2:
+               this.consoleName = "PS5";
+               break;
+           case 3:
+               this.consoleName = "Xbox Series X";
+               break;
+       }
+        
+        
+        this.name = name;
+    }
+    
+      public String toString()
+    {
+        return new String(name + " is a level " + nerdFactor + " nerd who plays " + consoleName + ".");
+    }
+    
+    /**
+       Harass this nerd.  Harassment is based upon the nerd's nerd factor.
+       Postcondition: A message indicating how to harass this nerd is printed.
+     */
+    @Override
+    public void harass()
+    {
+        System.out.println("You tell nerd " + name + getHarassment());
+    }
+    
+    @Override
+    public String getHarassment()
+    {
+        String harassment = null;
+        switch (console)
+        {
+            case 1:
+                harassment = new String("Too many bugs on the PC!");
+                break;
+            case 2:
+                harassment = new String("No Xbox game pass on your PlayStation!");
+                break;
+            case 3:
+                harassment = new String("You sure must enjoy seeing ads on your console!");
+                break;
+            default: // this should never happen!
+                System.out.println("Error: unrecognized error code!!!");
+                System.exit(2);
+        }
+        return harassment;
+    }
+    
+}
---
+++ Lab1.java
@@ -0,0 +1,128 @@
+
+package uhh.lab1;
+import java.util.*;
+
+/**
+ *
+ * @author devoncz
+ */
+public class Lab1 {
+    
+    /** Creates a new instance of Lab1 */
+    public Lab1() {
+    }
+    
+    
+    /**
+       Print a list of nerds.
+       @param A list containing GenericNerd objects.
+     */
+    public static void showNerdList(ArrayList<GenericNerd> list)
+    {
+        System.out.println();
+        System.out.println("************************************************");
+        System.out.println("*             List of Known Nerds              *");
+        System.out.println("************************************************");
+        System.out.println();
+        
+        if(list.size() == 0)
+        {
+            System.out.println("\tThere are no nerds.");
+        }
+        
+        for(int i = 0; i < list.size(); i++)
+        {
+            GenericNerd nerd = list.get(i);
+            // note implicit call to nerd.toString() below.
+            System.out.println("\t" + nerd);
+        }
+        
+        System.out.println();
+        System.out.println("************************************************");
+        System.out.println("*               End of Nerd List               *");
+        System.out.println("************************************************");
+        System.out.println();
+    }
+    
+    /**
+       Harass all of the nerds in the list.
+       @param A list containing GenericNerd objects.
+     */
+    public static void harassNerds(ArrayList<GenericNerd> list)
+    {
+        System.out.println();
+        System.out.println("************************************************");
+        System.out.println("*               Harassing Nerds                *");
+        System.out.println("************************************************");
+        System.out.println();
+        
+        if(list.size() == 0)
+        {
+            System.out.println("\tThere are no nerds to harass.");
+        }
+        
+        for(int i = 0; i < list.size(); i++)
+        {
+            GenericNerd nerd = list.get(i);
+            nerd.harass();
+        }
+        
+        System.out.println();
+        System.out.println("************************************************");
+        System.out.println("*   Finished Harassing Nerds (that was fun!)   *");
+        System.out.println("************************************************");
+        System.out.println();
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //System.out.println("<<< CS 420 Lab 1 Test Code. >>>");
+        
+        // List to keep a few nerds in
+        ArrayList<GenericNerd> nerdList = new ArrayList(10);
+        
+        // create a few nerds and add them to the list
+        GenericNerd nerd = new GenericNerd("Bugger McNosePicker", 9);
+        nerdList.add(nerd);
+        // This will generate a warning message:
+        nerd = new GenericNerd("Lame-o Lamerson", 12);
+        nerdList.add(nerd);
+        
+        
+        // POLYMORPHISM IN ACTION!!!
+        nerd = new Trekkie("Captain Spazz", 3,false,true);
+        nerdList.add(nerd);
+        nerd = new Trekkie("Lt. Commander Loser", 10,true,true);
+        nerdList.add(nerd);
+        // generate another warning (Klingon speaker of a low level)
+        nerd = new Trekkie("Deanna Troi fanboy", 6,true,false);
+        nerdList.add(nerd);
+        // round out the list with a few more various nerds
+        nerd = new GenericNerd("Baron Ron von Dorkenstein", 10);
+        nerdList.add(nerd);
+        nerd = new GenericNerd("Donnie Doofus", 8);
+        nerdList.add(nerd);
+        
+        
+        nerd = new GamerGeek("PC Gamer", 1, 8);
+        nerdList.add(nerd);
+        nerd = new GamerGeek("PS5 Gamer", 2, 10);
+        nerdList.add(nerd);
+        nerd = new GamerGeek("Xbox Gamer", 3, 3);
+        nerdList.add(nerd);
+        
+        
+        // print out a list of the nerds:
+        showNerdList(nerdList);
+        
+        // harass all of the nerds
+        harassNerds(nerdList);
+        
+    }
+    
+}
+
